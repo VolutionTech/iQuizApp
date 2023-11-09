d62b31d3011d7b202b7f24923e5237ac7e070616
Ali Murad <rxalimurad@gmail.com>
2023-11-09 19:08:19 +0500

    --- remove all project

--- lib/url.dart
+++
@@ -1,8 +0,0 @@
-const url = 'https://quizvolutiontech.000webhostapp.com/_newCategoryID_.json';
-const categoryPlaceholder = '_newCategoryID_';
-String getCategoryURL(String category) {
-  return url.replaceAll(categoryPlaceholder, category);
-}
-
-const baseURL = "https://iquiz.onrender.com";
-const fetchCategoryEndPoint = "/api/v1/quiz";
